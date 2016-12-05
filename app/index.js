94cb11afa2d6a47a7a56a41fe86644429e0ae7b4
kmee0507 <kcmee@uvm.edu>
2016-12-05 17:56:17 -0500

    Build 3
    Game now will end when all ships are sunk.  Added arrays to keep track
    of spots for ship locations

--- app/index.js
+++ app/index.js
@@ -5,7 +5,10 @@ const board = require('./board.js');
 const pieces = require('./pieces.js');
 require('events').EventEmitter.prototype._maxListeners = 100;
 var gameBoard = board.createBoard();
- var counter = 1;
+var counter = 1;
+var shipCount = 0;
+var occupiedSpots = [];
+var currentlyOccupiedSpots = [];
 var gamePieces = pieces.createPieces();
     
 
@@ -20,7 +23,32 @@ function battleShipSpots(){
         
         rl.question('Enter spot for the 2 ship (A-J 1-10):', (answer) => {
           // TODO: Log the answer in a database
-          
+          shipCount+=1;
+          currentlyOccupiedSpots.push(answer.toUpperCase());
+          if(shipCount == 2 ){
+              occupiedSpots.push(currentlyOccupiedSpots);
+              currentlyOccupiedSpots = [];
+          }
+          if(shipCount == 5){
+              occupiedSpots.push(currentlyOccupiedSpots);
+              currentlyOccupiedSpots = [];
+          }
+          if(shipCount == 8){
+              occupiedSpots.push(currentlyOccupiedSpots);
+              
+              
+              currentlyOccupiedSpots = [];
+          }
+          if(shipCount == 12){
+              occupiedSpots.push(currentlyOccupiedSpots);
+              
+              currentlyOccupiedSpots = [];
+          }
+          if(shipCount == 17){
+              occupiedSpots.push(currentlyOccupiedSpots);
+            
+              currentlyOccupiedSpots = [];
+          }
           for(var i = 1;i<=100;i++){
               if(gameBoard[i] == answer.toUpperCase()){
                   gameBoard[i] = "[]";
@@ -42,6 +70,7 @@ function battleShipSpots(){
             console.log("");
             board.initializeBoard(gameBoard);
             guess();
+            
         });
         
 }
@@ -54,13 +83,28 @@ function guess(){
         });
         console.log("");
         rl.question('Guess a location (A-J 1-10): ', (guessedLocation) => {
-            var split = guessedLocation.split("");
-            
+            var split = guessedLocation.toUpperCase().split("");
+            var indexOfArray = 0;
+            for(var x = 0;x<occupiedSpots.length;x++){
+                for(var y = 0;y<occupiedSpots[x];y++){
+                    if(occupiedSpots[x][y] == guessedLocation.toUpperCase()){
+                        indexOfArray = x;
+                        break;
+                    }    
+                }    
+            }
             if(split[0] == "A"){
                 if(gameBoard[parseInt(split[1])] == "[]"){
                     console.log("HIT");
+                    var index = occupiedSpots[indexOfArray].indexOf(guessedLocation.toUpperCase());
+                    
+                    occupiedSpots[indexOfArray].splice(index,1);
+                    if(occupiedSpots[indexOfArray] == 0){
+                        occupiedSpots.splice(indexOfArray,1);
+                        console.log("SUNK!!!")
+                    }
                     gameBoard[parseInt(split[1])] = "OO";
-                    //board.initializeBoard(gameBoard);
+                    
                 }
                 else{
                     console.log("MISS");
@@ -71,6 +115,14 @@ function guess(){
             else if(split[0] == "B"){
                 if(gameBoard[parseInt(split[1]) + 10] == "[]"){
                     console.log("HIT");
+                    var index = occupiedSpots[indexOfArray].indexOf(guessedLocation.toUpperCase());
+                    
+                    occupiedSpots[indexOfArray].splice(index,1);
+                    if(occupiedSpots[indexOfArray] == 0){
+                        occupiedSpots.splice(indexOfArray,1);
+                        console.log("SUNK!!!")
+                    }
+                    gameBoard[parseInt(split[1])+10] = "OO";
                 }
                 else{
                     console.log("MISS");
@@ -81,6 +133,14 @@ function guess(){
             else if(split[0] == "C"){
                 if(gameBoard[parseInt(split[1]) + 20] == "[]"){
                     console.log("HIT");
+                    var index = occupiedSpots[indexOfArray].indexOf(guessedLocation.toUpperCase());
+                    
+                    occupiedSpots[indexOfArray].splice(index,1);
+                    if(occupiedSpots[indexOfArray] == 0){
+                        occupiedSpots.splice(indexOfArray,1);
+                        console.log("SUNK!!!")
+                    }
+                    gameBoard[parseInt(split[1])+20] = "OO";
                 }
                 else{
                     console.log("MISS");
@@ -91,6 +151,14 @@ function guess(){
             else if(split[0] == "D"){
                 if(gameBoard[parseInt(split[1]) + 30] == "[]"){
                     console.log("HIT");
+                    var index = occupiedSpots[indexOfArray].indexOf(guessedLocation.toUpperCase());
+                    
+                    occupiedSpots[indexOfArray].splice(index,1);
+                    if(occupiedSpots[indexOfArray] == 0){
+                        occupiedSpots.splice(indexOfArray,1);
+                        console.log("SUNK!!!")
+                    }
+                    gameBoard[parseInt(split[1])+30] = "OO";
                 }
                 else{
                     console.log("MISS");
@@ -101,6 +169,14 @@ function guess(){
             else if(split[0] == "E"){
                 if(gameBoard[parseInt(split[1]) + 40] == "[]"){
                     console.log("HIT");
+                    var index = occupiedSpots[indexOfArray].indexOf(guessedLocation.toUpperCase());
+                    
+                    occupiedSpots[indexOfArray].splice(index,1);
+                    if(occupiedSpots[indexOfArray] == 0){
+                        occupiedSpots.splice(indexOfArray,1);
+                        console.log("SUNK!!!")
+                    }
+                    gameBoard[parseInt(split[1])+40] = "OO";
                 }
                 else{
                     console.log("MISS");
@@ -111,6 +187,14 @@ function guess(){
             else if(split[0] == "F"){
                 if(gameBoard[parseInt(split[1]) + 50] == "[]"){
                     console.log("HIT");
+                    var index = occupiedSpots[indexOfArray].indexOf(guessedLocation.toUpperCase());
+                    
+                    occupiedSpots[indexOfArray].splice(index,1);
+                    if(occupiedSpots[indexOfArray] == 0){
+                        occupiedSpots.splice(indexOfArray,1);
+                        console.log("SUNK!!!")
+                    }
+                    gameBoard[parseInt(split[1])+50] = "OO";
                 }
                 else{
                     console.log("MISS");
@@ -121,6 +205,14 @@ function guess(){
             else if(split[0] == "G"){
                 if(gameBoard[parseInt(split[1]) + 60] == "[]"){
                     console.log("HIT");
+                    var index = occupiedSpots[indexOfArray].indexOf(guessedLocation.toUpperCase());
+                    
+                    occupiedSpots[indexOfArray].splice(index,1);
+                    if(occupiedSpots[indexOfArray] == 0){
+                        occupiedSpots.splice(indexOfArray,1);
+                        console.log("SUNK!!!")
+                    }
+                    gameBoard[parseInt(split[1])+60] = "OO";
                 }
                 else{
                     console.log("MISS");
@@ -131,6 +223,14 @@ function guess(){
             else if(split[0] == "H"){
                 if(gameBoard[parseInt(split[1]) + 70] == "[]"){
                     console.log("HIT");
+                    var index = occupiedSpots[indexOfArray].indexOf(guessedLocation.toUpperCase());
+                    
+                    occupiedSpots[indexOfArray].splice(index,1);
+                    if(occupiedSpots[indexOfArray] == 0){
+                        occupiedSpots.splice(indexOfArray,1);
+                        console.log("SUNK!!!")
+                    }
+                    gameBoard[parseInt(split[1])+70] = "OO";
                 }
                 else{
                     console.log("MISS");
@@ -141,6 +241,14 @@ function guess(){
             else if(split[0] == "I"){
                 if(gameBoard[parseInt(split[1]) + 80] == "[]"){
                     console.log("HIT");
+                    var index = occupiedSpots[indexOfArray].indexOf(guessedLocation.toUpperCase());
+                    
+                    occupiedSpots[indexOfArray].splice(index,1);
+                    if(occupiedSpots[indexOfArray] == 0){
+                        occupiedSpots.splice(indexOfArray,1);
+                        console.log("SUNK!!!")
+                    }
+                    gameBoard[parseInt(split[1])+80] = "OO";
                 }
                 else{
                     console.log("MISS");
@@ -151,12 +259,24 @@ function guess(){
             else if(split[0] == "J"){
                 if(gameBoard[parseInt(split[1]) + 90] == "[]"){
                     console.log("HIT");
+                    var index = occupiedSpots[indexOfArray].indexOf(guessedLocation.toUpperCase());
+                    
+                    occupiedSpots[indexOfArray].splice(index,1);
+                    if(occupiedSpots[indexOfArray] == 0){
+                        occupiedSpots.splice(indexOfArray,1);
+                        console.log("SUNK!!!")
+                    }
+                    gameBoard[parseInt(split[1])+90] = "OO";
                 }
                 else{
                     console.log("MISS");
                     gameBoard[parseInt(split[1]) + 90] = "XX";
                         
                 }
+            }
+            if(occupiedSpots.length == 0){
+                console.log("\n\nGAME OVER YOU WIN!!!!!!");
+                process.exit()
             }
             board.initializeBoard(gameBoard);
             guess();
@@ -171,5 +291,6 @@ function introduction(){
     console.log("[] - occupied space");
     console.log("\n\n");
     battleShipSpots();
+    
 }
 introduction();
